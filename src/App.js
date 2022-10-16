23f9caf62e046933c9abaf9eb83765baf6dbb612
EnniKelloniemi <enni.kelloniemi@gmail.com>
2022-10-16 12:06:54 +0300

    Melkein valmis

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { useState } from 'react';
 import './App.css';
 import axios from 'axios';
-import TableTrains from './TableTrains'
 const TRAINS_URL = 'https://rata.digitraffic.fi/api/v1/live-trains/station/'
 const STATION_URL = 'https://rata.digitraffic.fi/api/v1/metadata/stations'
 
@@ -10,7 +9,9 @@ function App() {
 
   const [stationName, setStationName] = useState('')
   const [stationOfArrival, setStationOfAffival] = useState('')
-
+  const [departure, setdeparture] = useState([])
+  const [trainNumbers, setTrainNumbers] = useState([])
+  const [arrivalTimes, setArrivalTimes] = useState([])
   let departureStationShortCode = ''
   let arrivalStationShortCode = ''
   let departureTime = []
@@ -18,83 +19,103 @@ function App() {
   let arrivalTime = []
 
   const getTrains = async () => {
-    console.log(stationName)
     try {
       const response = await axios.get(STATION_URL)
       const dataStations = await response.data
-      /*Haetaan lähtöaseman asemakoodi. Tämä ei ole vielä huomioinut niitä kaupunkeja, joilla on monta lähtöasemaa*/
+      /*Haetaan lähtöaseman asemakoodi. Tämä ei ole vielä huomioinut niitä kaupunkeja, joilla on monta lähtöasemaa(esim Oulu)*/
       dataStations.forEach(element => {
         if (element.stationName === stationName) {
-          console.log("if-lauseessa")
-          console.log(element.stationName)
-          console.log(element.stationShortCode)
           departureStationShortCode = element.stationShortCode
-          console.log(departureStationShortCode)
         }
       });
       /*Haetaan saapumisaseman asemakoodi */
       dataStations.forEach(element => {
         if (element.stationName === stationOfArrival) {
-          console.log("if-lauseessa2")
-          console.log(element.stationName)
-          console.log(element.stationShortCode)
           arrivalStationShortCode = element.stationShortCode
-          console.log(arrivalStationShortCode)
         }
       });
 
-        /*Hakee junan numerot ja lähtö- ja saapumisajan */
+      /*Hakee junan numerot ja lähtö- ja saapumisajan */
       const getTrainsResponse = await axios.get(TRAINS_URL + departureStationShortCode + '/' + arrivalStationShortCode)
-      console.log(getTrainsResponse)
       const dataGetTrains = getTrainsResponse.data
-      console.log(dataGetTrains)
       dataGetTrains.forEach(element => {
-        console.log('hep', element.timeTableRows[0])
         element.timeTableRows.forEach(i => {
-          /*console.log('i on', i)*/
           /*Lisätää listaan departureTime junan lähtöaika ja listaan trainNumber junan numerot  */
           if (i.stationShortCode === departureStationShortCode &&
             i.type === "DEPARTURE") {
-            departureTime.push(i.scheduledTime)
+            departureTime.push(new Date(i.scheduledTime).toLocaleTimeString())
             trainNumber.push(i.trainNumber)
-            console.log(departureTime)
-            console.log(trainNumber)
-            return departureTime, trainNumber
           }
           /*Lisätään listaan arrivalTime junan saapumisaika */
           if (i.stationShortCode === arrivalStationShortCode &&
             i.type === "ARRIVAL") {
-            arrivalTime.push(i.scheduledTime)
-            console.log(arrivalTime)
-            return arrivalTime
+            arrivalTime.push(new Date(i.scheduledTime).toLocaleTimeString())
+
           }
         })
+        setdeparture(departureTime)
+        setTrainNumbers(trainNumber)
+        setArrivalTimes(arrivalTime)
+        /*console.log(departureTime)*/
       });
     } catch (err) {
       alert(err);
     }
-    return departureTime, trainNumber,arrivalTime
   }
 
-console.log()
   return (
-    <>
-      <div><h1>Juna</h1></div>
+    <div className='container'>
       <div>
-        <label>Valitse lähtö asema</label>
-        <input type="text" value={stationName} onChange={e => setStationName(e.target.value)}></input>
-        <label>Kirjoita määränpääasema</label>
-        <input type="text" value={stationOfArrival} onChange={e => setStationOfAffival(e.target.value)}></input>
+        <h1>Juna</h1>
+        <p>Kirjoita lähtöasema ja määränpää, niin saat junat, jotka menevät 24 tunnin sisällä asemien välillä</p>
+      </div>
+      <div>
+        <div>
+          <label>Kirjoita lähtöasema</label>
+          <input type="text" value={stationName} onChange={e => setStationName(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Kirjoita määränpääasema</label>
+          <input type="text" value={stationOfArrival} onChange={e => setStationOfAffival(e.target.value)}></input>
+        </div>
         <button onClick={() => getTrains({ stationName }, { stationOfArrival })} >Hae
         </button>
-        <output>
-          <TableTrains
-          departureStation= {stationName}
-          arrivalStation={stationOfArrival}/>
-        </output>
 
+        <table>
+          <thead>
+            <tr>
+              <th>Lähtöaika</th>
+              <th>Juna</th>
+              <th>Saapumisaika</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <dl>{departure.map((time, index) => (
+                  <dt key={index}>{time}</dt>
+                ))}
+                </dl>
+              </td>
+              <td>
+                <dl>
+                  {trainNumbers.map((train, index) => (
+                    <dt key={index}>{train}</dt>
+                  ))}
+                </dl>
+              </td>
+              <td>
+                <dl>
+                  {arrivalTimes.map((arrivalTime, index) => (
+                    <dt key={index}>{arrivalTime}</dt>
+                  ))}
+                </dl>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </>
+    </div>
   );
 
 }
